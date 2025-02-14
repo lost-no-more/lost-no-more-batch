655f24152b481da03d91df1d2fff6fd70285a952
dongyun <ehddbs452100@naver.com>
2025-02-14 23:18:25 +0900

    [chore] #1 init project

---
+++ src/main/java/lost_no_more/lost_no_more_batch/LostNoMoreBatchApplication.java
@@ -0,0 +1,13 @@
+package lost_no_more.lost_no_more_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostNoMoreBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostNoMoreBatchApplication.class, args);
+	}
+
+}
