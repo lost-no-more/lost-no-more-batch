c0097157be602cd400f5252cbc7a2b55a465551c
dongyun <ehddbs452100@naver.com>
2025-02-23 17:54:58 +0900

    [feat] #5 메타 db, 데이터 db 분리

---
+++ src/main/java/lost_no_more/lost_no_more_batch/global/db/config/MetaDBConfig.java
@@ -0,0 +1,29 @@
+package lost_no_more.lost_no_more_batch.global.db.config;
+
+import javax.sql.DataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class MetaDBConfig {
+
+    @Primary
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource-meta")
+    public DataSource metaDBSource() {
+
+        return DataSourceBuilder.create().build();
+    }
+
+    @Primary
+    @Bean
+    public PlatformTransactionManager metaTransactionManager() {
+
+        return new DataSourceTransactionManager(metaDBSource());
+    }
+}
