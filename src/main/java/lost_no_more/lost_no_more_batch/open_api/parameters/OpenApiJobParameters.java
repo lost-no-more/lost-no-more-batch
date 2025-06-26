de8aa7c0c124d84c9edc389c7df218f2151bdbff
bronze_yun_4521 <ehddbs452100@naver.com>
2025-06-26 09:36:34 +0900

    [refactor] #9 공공데이터 포털 배치 파라미터 설정 및 검증

--- src/main/java/lost_no_more/lost_no_more_batch/open_api/parameters/OpenApiJobParameters.java
+++ src/main/java/lost_no_more/lost_no_more_batch/open_api/parameters/OpenApiJobParameters.java
@@ -1,51 +1,25 @@
 package lost_no_more.lost_no_more_batch.open_api.parameters;
 
-import lombok.Getter;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import lombok.Getter;
 
 @Getter
 @StepScope
 @Component
 public class OpenApiJobParameters {
 
-    private String startDate;
-    private String endDate;
-    private Integer pageNo;
-    private Integer numOfRows;
-
     @Value("#{jobParameters['START_DATE']}")
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
+    private String startDate;
 
     @Value("#{jobParameters['END_DATE']}")
-    public void setEndDate(String endDate) {
-        this.endDate = endDate;
-    }
+    private String endDate;
 
     @Value("#{jobParameters['PAGE_NO']}")
-    public void setPageNo(String pageNo) {
-        this.pageNo = Integer.valueOf(pageNo);
-    }
+    private Long pageNo;
 
     @Value("#{jobParameters['NUM_OF_ROWS']}")
-    public void setNumOfRows(String numOfRows) {
-        this.numOfRows = Integer.valueOf(numOfRows);
-    }
-
-    public Integer getCurrentPage(ChunkContext chunkContext) {
-        Integer currentPage = (Integer) chunkContext.getStepContext()
-                .getStepExecution()
-                .getExecutionContext()
-                .get("PAGE_NO");
-
-        if (currentPage == null) {
-            currentPage = this.pageNo;
-        }
-
-        return currentPage;
-    }
+    private Long numOfRows;
 }
