29316bc3f0ab00f904883ab48e38ab2ec5d8eea0
bronze_yun_4521 <ehddbs452100@naver.com>
2025-06-26 09:33:23 +0900

    [refactor] #9 location 테이블 인덱스 설정

--- src/main/java/lost_no_more/lost_no_more_batch/item/domain/Location.java
+++ src/main/java/lost_no_more/lost_no_more_batch/item/domain/Location.java
@@ -5,24 +5,29 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lost_no_more.lost_no_more_batch.global.domain.BaseEntity;
 
 @Getter
-@Table(name = "location")
+@Table(name = "location",
+    indexes = {
+        @Index(name = "idx_location_name", columnList = "location_name", unique = true)
+    })
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Location {
+public class Location extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "location_id")
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "location_name", nullable = false)
     private String name;
 
     @Column(name = "latitude", nullable = false)
