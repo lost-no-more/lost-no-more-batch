e9125b0c97a387eb94f71b88076f3107d93086e0
bronze_yun_4521 <ehddbs452100@naver.com>
2025-06-29 19:31:38 +0900

    [fix] #9 테스트 설정 추가

--- src/test/java/lost_no_more/lost_no_more_batch/LostNoMoreBatchApplicationTests.java
+++ src/test/java/lost_no_more/lost_no_more_batch/LostNoMoreBatchApplicationTests.java
@@ -2,8 +2,10 @@ package lost_no_more.lost_no_more_batch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LostNoMoreBatchApplicationTests {
 
 	@Test
